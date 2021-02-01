898e1ea3779df8baef3cc1330982dc006f428625
Donalson Javier <donalson1517@gmail.com>
2021-01-31 20:22:39 -0600

    Rutas de Tortillas
    Las rutas del modulo de tortillas ya cuentas con su crud completo

--- src/routes/rutas.js
+++ src/routes/rutas.js
@@ -8,9 +8,9 @@ router.get('/', async (req, res) => {
     res.send('Bienvenido a la API del proyecto tortillas de harina')
 })
 
-//Ruta de llamado de Clientes
-router.get('/Clientes', async (req, res) => {
-    conexion.query('SELECT * FROM clientes WHERE Activo = 1', (err, rows, fields) => {
+//Ruta de llamado de Tortillas
+router.get('/Tortillas', async (req, res) => {
+    conexion.query('SELECT * FROM tortillas WHERE Activa = 1', (err, rows, fields) => {
         if(!err){
             res.json(rows);
         }else{
@@ -19,10 +19,10 @@ router.get('/Clientes', async (req, res) => {
     });
 });
 
-//Ruta de llamado de Clientes por id
-router.get('/Clientes/:id', async (req, res) => {
+//Ruta de llamado de Tortillas por id
+router.get('/Tortillas/:id', async (req, res) => {
     const { id } = req.params;
-    conexion.query('SELECT * FROM clientes WHERE IdCliente = ?', [id], (err, rows, fields) => {
+    conexion.query('SELECT * FROM tortillas WHERE IdTortilla = ?', [id], (err, rows, fields) => {
         if(!err){
             res.json(rows[0]);
         }else{
@@ -31,39 +31,39 @@ router.get('/Clientes/:id', async (req, res) => {
     });
 });
 
-//Ruta de Creacion de Clientes
-router.post('/Clientes', async (req, res) => {
-    const { Nombres, Apellidos, Direccion, Telefono, Nit, Adelanto, Debe, Observacion, Foto} = req.body;
-    const SetenciaSQL = 'INSERT INTO `clientes` (`IdCliente`, `Nombres`, `Apellidos`, `Direccion`, `Telefono`, `Nit`, `Adelanto`, `Debe`, `Observacion`, `Foto`, `FC`, `FE`, `Activo`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, current_timestamp(), NULL, 1)';
-    conexion.query(SetenciaSQL,[Nombres, Apellidos, Direccion, Telefono, Nit, Adelanto, Debe, Observacion, Foto], (err, rows, fields) => {
+//Ruta de Creacion de Tortilla
+router.post('/Tortillas', async (req, res) => {
+    const { Descripcion, Precio, Foto} = req.body;
+    const SetenciaSQL = 'INSERT INTO `tortillas` (`IdTortilla`, `Descripcion`, `Precio`, `Foto`, `FC`, `FE`, `Activa`) VALUES (NULL, ?, ?, ?, current_timestamp(), NULL, 1)';
+    conexion.query(SetenciaSQL,[Descripcion, Precio, Foto], (err, rows, fields) => {
         if(!err){
-            res.json({Status: 'Cliente Registrado'});
+            res.json({Status: 'Tortilla Registrada'});
         }else{
             console.log(err)
         }
     });
 });
 
-//Ruta de Acutalizacion de Clientes
-router.put('/Clientes/:id', async (req, res) =>{
-    const { Nombres, Apellidos, Direccion, Telefono, Nit, Adelanto, Debe, Observacion, Foto, Activo} = req.body;
+//Ruta de Acutalizacion de Tortillas
+router.put('/Tortillas/:id', async (req, res) =>{
+    const { Descripcion, Precio, Foto, Activo} = req.body;
     const { id } = req.params;
-    const SetenciaSQL = 'UPDATE `clientes` SET `Nombres` = ?, `Apellidos` = ?, `Direccion` = ?, `Telefono` = ?, `Nit` = ?, `Adelanto` = ?, `Debe` = ?, `Observacion` = ?, `Foto` = ?, `FE` = CURRENT_TIMESTAMP(), `Activo` = ? WHERE `IdCliente` = ?';
-    conexion.query(SetenciaSQL, [Nombres, Apellidos, Direccion, Telefono, Nit, Adelanto, Debe, Observacion, Foto, Activo, id], (err, rows, fields) => {
+    const SetenciaSQL = 'UPDATE `tortillas` SET `Descripcion` = ?, `Precio` = ?, `Foto` = ?, `FE` = CURRENT_TIMESTAMP(), `Activa` = ? WHERE `IdTortilla` = ?';
+    conexion.query(SetenciaSQL, [Descripcion, Precio, Foto, Activo, id], (err, rows, fields) => {
         if(!err){
-            res.json({Status: "Cliente Acutalizado"});
+            res.json({Status: "Tortilla Acutalizada"});
         }else{
             console.log(err)
         }
     });
 });
 
-//Ruta de Borrado de Clientes
-router.delete('/Clientes/:id', async (req, res) => {
+//Ruta de Borrado de Tortillas
+router.delete('/Tortillas/:id', async (req, res) => {
     const { id } = req.params;
-    conexion.query('UPDATE `clientes` SET `Activo` = 0 WHERE `IdCliente` = ?', [id], (err, rows, fields) => {
+    conexion.query('UPDATE `tortillas` SET `Activa` = 0 WHERE `IdTortilla` = ?', [id], (err, rows, fields) => {
         if(!err){
-            res.json({Status: "Cliente marcado como inactivo"});
+            res.json({Status: "Tortilla marcado como inactiva"});
         }else{
             console.log(err)
         }
