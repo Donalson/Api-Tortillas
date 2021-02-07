4933e88c108a45efcef9b1033d01f007f4ea9642
Donalson Javier <donalson1517@gmail.com>
2021-02-06 22:49:01 -0600

    Uso de CORS
    Se instalo el paquete de cors y se establecio un rango limitado de origenes que pueden acceder al sitio

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require ('cors');
 const app = express();
 
 //Configuracion
@@ -6,6 +7,22 @@ app.set('port', process.env.PORT || 3000);
 
 //Middlewares
 app.use(express.json());
+//CORS ABIERTAS
+//app.use(cors());
+//CORS RESTRINGIDAS
+var permitidos = ['http://localhost:8080', 'http://192.168.1.4:8080'];
+app.use(cors({
+  origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(permitidos.indexOf(origin) === -1){
+      var msg = 'Las politicas CORS para este sitio no permiten acceso para el origen especificado.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //Rutas
 app.use(require('./routes/rutas'));
